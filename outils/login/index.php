b8f9af74e70a6299fed728e7be5f5fdba7f98ad3
Maxime <maxime-bonnet42@orange.fr>
2022-03-08 08:47:20 +0100

    modif requete

---
+++ outils/login/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <title>Accueil</title>
+</head>
+<body>
+    <h1>Welcome Wold !</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae, expedita quia ab perspiciatis fugit magni, ex similique suscipit maxime non, blanditiis magnam harum impedit provident corporis earum dolor natus atque?</p>
+    <div id="center_button">
+    <button onclick="location.href='login.php'">Login</button>
+</body>
+</html>
--- outils/test/index.php
+++ outils/test/index.php
@@ -29,52 +29,54 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="mt-5 mb-3 clearfix">
-                        <h2 class="pull-left">Données</h2>
-                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Ajouter une données</a>
+                        <h2 class="pull-left">Employees Details</h2>
+                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Employee</a>
                     </div>
                     <?php
                     // Include config file
                     require_once "config.php";
                     
                     // Attempt select query execution
-                    $sql = "SELECT * FROM ETAT";
-                    if($result = mysqli_query($link, $sql)){
-                        if(mysqli_num_rows($result) > 0){
+                    $sql = "SELECT * FROM employees";
+                    if($result = $pdo->query($sql)){
+                        if($result->rowCount() > 0){
                             echo '<table class="table table-bordered table-striped">';
                                 echo "<thead>";
                                     echo "<tr>";
                                         echo "<th>#</th>";
-                                        echo "<th>ETAT</th>";
-                                        echo "<th>Ticket</th>";
+                                        echo "<th>Name</th>";
+                                        echo "<th>Address</th>";
+                                        echo "<th>Salary</th>";
                                         echo "<th>Action</th>";
                                     echo "</tr>";
                                 echo "</thead>";
                                 echo "<tbody>";
-                                while($row = mysqli_fetch_array($result)){
+                                while($row = $result->fetch()){
                                     echo "<tr>";
-                                        echo "<td>" . $row['ET_ID'] . "</td>";
-                                        echo "<td>" . $row['TIC_LIB'] . "</td>";
-                                        echo "<td>" . $row['ET_LIB'] . "</td>";
+                                        echo "<td>" . $row['id'] . "</td>";
+                                        echo "<td>" . $row['name'] . "</td>";
+                                        echo "<td>" . $row['address'] . "</td>";
+                                        echo "<td>" . $row['salary'] . "</td>";
                                         echo "<td>";
-                                            echo '<a href="read.php?id='. $row['ET_ID'] .'" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
-                                            echo '<a href="update.php?id='. $row['ET_ID'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
-                                            echo '<a href="delete.php?id='. $row['ET_ID'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                            echo '<a href="read.php?id='. $row['id'] .'" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                                            echo '<a href="update.php?id='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
+                                            echo '<a href="delete.php?id='. $row['id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
                                         echo "</td>";
                                     echo "</tr>";
                                 }
                                 echo "</tbody>";                            
                             echo "</table>";
                             // Free result set
-                            mysqli_free_result($result);
+                            unset($result);
                         } else{
                             echo '<div class="alert alert-danger"><em>No records were found.</em></div>';
                         }
                     } else{
                         echo "Oops! Something went wrong. Please try again later.";
                     }
- 
+                    
                     // Close connection
-                    mysqli_close($link);
+                    unset($pdo);
                     ?>
                 </div>
             </div>        
